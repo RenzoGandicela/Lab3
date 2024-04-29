6fc848a8dc4263bf63e3caefe0f027c5faa9ddb8
RenzoGandicela <RENZOGANDICELA.23@ichat.sp.edu.sg>
2024-04-29 16:19:00 +0800

    Test_bmi.py file committed!

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,10 +1,10 @@
-import Lab2.bmi as BMI
+from Lab2.bmi import calculate_bmi
 
 def test_bmi_normal_weight():
-    assert (BMI.calculate_bmi(1.60,50) == 0)
+    assert (calculate_bmi(1.60,50) == 0)
 
 def test_bmi_under_weight():
-    assert (BMI.calculate_bmi(1.60,30) == -1)
+    assert (calculate_bmi(1.60,30) == -1)
 
 def test_bmi_over_weight():
-    assert (BMI.calculate_bmi(1.73,80) == 1)
+    assert (calculate_bmi(1.73,80) == 1)
